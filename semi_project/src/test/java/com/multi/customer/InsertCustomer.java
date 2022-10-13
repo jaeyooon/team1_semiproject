12219cfc40f593dee1b291751c1a64d46a5a62dc
jaeyooon <jycho0208@gmail.com>
2022-10-13 10:29:34 +0900

    modify Customer xml file & add CRUD files

---
+++ semi_project/src/test/java/com/multi/customer/InsertCustomer.java
@@ -0,0 +1,26 @@
+package com.multi.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.multi.dto.CustomerDTO;
+import com.multi.service.CustomerService;
+
+@SpringBootTest
+public class InsertCustomer {
+	
+	@Autowired
+	CustomerService service;
+	
+	@Test
+	void contextLoads() {
+		CustomerDTO cust = new CustomerDTO("id05", "pwd05", "홍말숙", "010-5789-1234", "1996-01-01", "F", "경기도 성남시 분당구", 1, "10% 쿠폰");
+		try {
+			service.register(cust);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("ok");
+	}
+}
---
+++ semi_project/src/test/java/com/multi/customer/UpdateCustomer.java
@@ -0,0 +1,28 @@
+package com.multi.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.multi.dto.CategoryDTO;
+import com.multi.dto.CustomerDTO;
+import com.multi.service.CategoryService;
+import com.multi.service.CustomerService;
+
+@SpringBootTest
+class UpdateCustomer {
+	
+	@Autowired
+	CustomerService service;
+	
+	@Test
+	void contextLoads() {
+		CustomerDTO cust = new CustomerDTO("id03", "pwd03", "조재윤", "010-4545-7676", "1998-03-22", "F", "강원도 강릉시", 3, "20% 쿠폰");
+		try {
+			service.modify(cust);
+			System.out.println("OK");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
